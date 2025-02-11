f78d4c3dea398cfb278fb98bffca316cbffc8986
frmauro <frmauro8@gmail.com>
2025-02-11 17:18:45 -0300

    several refactoring

--- WebClient/Pages/Checkout/Index.cshtml.cs
+++ WebClient/Pages/Checkout/Index.cshtml.cs
@@ -12,12 +12,55 @@ namespace WebClient.Pages.Checkout
         public CartVO? CartVo { get; set; } = new CartVO();
 
         [BindProperty]
-        public string? ErrorMessage  { get; set; }
+        public string? ErrorMessage { get; set; }
 
         public async Task OnGet()
         {
-            var usarId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
-            var cart = await cartService.FindCartByUserId(usarId);
+            var userId = User.Claims.FirstOrDefault(u => u.Type == "sub")?.Value;
+            if (userId != null)
+            {
+                CartVo = await GetCartVo(userId);
+            }
+        }
+
+
+        public async Task<IActionResult> OnPost()
+        {
+            var userId = User.Claims.FirstOrDefault(u => u.Type == "sub")?.Value;
+
+            if (userId == null)
+            {
+                ErrorMessage = "User not found. Please login again.";
+                return Page();
+            }
+            try
+            {
+                var response = await cartService.Checkout(CartVo?.CartHeader!);
+
+                if (response != null && response.GetType() == typeof(string))
+                {
+                    ErrorMessage = response!.ToString();
+                    var cart = await GetCartVo(userId);
+                    CartVo = cart;
+                    return Page();
+                }
+                else if (response != null)
+                {
+                    return Redirect("/Cart/Confirmation");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = $"An error occurred: {ex.Message}";
+            }
+            return Page();
+        }
+
+
+
+        private async Task<CartVO> GetCartVo(string userId)
+        {
+            var cart = await cartService.FindCartByUserId(userId);
 
             if (cart.CartHeader != null)
             {
@@ -28,26 +71,7 @@ namespace WebClient.Pages.Checkout
                 cart.CartHeader.PurshaseAmount -= cart.CartHeader.DiscountTotal;
             }
 
-            CartVo = cart;
-        }
-
-
-        public async Task<IActionResult> OnPost()
-        {
-            var usarId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
-            var response = await cartService.Checkout(CartVo?.CartHeader!);
-
-            if (response != null && response.GetType() == typeof(string))
-            {
-                ErrorMessage = response!.ToString();
-                return Page();
-            }
-            else if (response != null)
-            {
-                return Redirect("/Cart/Confirmation");
-            }
-
-            return Page();
+            return cart;
         }
     }
 }
